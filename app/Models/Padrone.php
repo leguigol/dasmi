1102284e3e85d443ffa04e9c12053e3cbfdbf43e
legui <leguigol@hotmail.com>
2023-10-06 20:36:02 -0300

    primer commit

---
+++ app/Models/Padrone.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Padrone extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        'id',
+        'afiliado',
+        'apellido',
+        'nombres',
+        'documento',
+        'cuil',
+        'added',
+        'alta',
+        'padron',
+        'zona',
+        'titular',
+        'fechanac',
+        'sexo',
+        'parentesco_id',
+        'altapadron',
+        'bajapadron',
+        'estado',
+        'plan_id',
+        'convenio_id',
+        'centro_id'
+    ];
+
+    public function convenio(){
+        return $this->belongsTo(Convenio::class, 'convenio_id');
+    }
+}
