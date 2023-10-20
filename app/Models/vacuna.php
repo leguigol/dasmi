e911ded59ce23743de9a8bd1cc7c33702ca6145e
legui <leguigol@hotmail.com>
2023-10-20 12:46:00 -0300

    vacunas ok

--- app/Models/vacuna.php
+++ app/Models/vacuna.php
@@ -13,24 +13,14 @@ class vacuna extends Model
         'edad',
         'vacuna',
     ];    
-<<<<<<< HEAD
     public function estaSeleccionada($valor, $valor2, $padronId)
     {
         return $this->vacunasPaciente()->where('vacuna_value', $valor)->where('vacuna_id',$valor2)->where('padron_id', $padronId)->exists();
-=======
-
-    public function estaSeleccionada($valor, $padronId)
-    {
-        return $this->vacunasPaciente()->where('vacuna_value', $valor)->where('padron_id', $padronId)->exists();
->>>>>>> e7e78e6667078f8e3ee33ad189a290d9818818b9
-    }
+    }    
 
     public function vacunasPaciente()
     {
         return $this->hasMany(VacunasPaciente::class);
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> e7e78e6667078f8e3ee33ad189a290d9818818b9
 }
